ea45454fd5b5d79fe82d7bdfb20fdf5d014845cf
김지윤 <50611288+kjy00@users.noreply.github.com>
2022-02-14 21:49:22 +0900

    Create ft_strncmp.c

---
+++ ft_strncmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyokim <jiyokim@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 16:39:43 by jiyokim           #+#    #+#             */
+/*   Updated: 2022/01/12 16:39:45 by jiyokim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*tmp1;
+	unsigned char	*tmp2;
+
+	i = 0;
+	tmp1 = (unsigned char *)s1;
+	tmp2 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	while (i < n && (tmp1[i] || tmp2[i]))
+	{
+		if (tmp1[i] != tmp2[i])
+			return ((int)(tmp1[i] - tmp2[i]));
+		i++;
+	}
+	return (0);
+}
