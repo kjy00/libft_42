e825a9e15b3423bf9c8361d0064f3b8ee24cace5
김지윤 <50611288+kjy00@users.noreply.github.com>
2022-02-16 19:35:36 +0900

    Add files via upload

---
+++ ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyokim <jiyokim@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/19 17:35:47 by jiyokim           #+#    #+#             */
+/*   Updated: 2022/01/12 11:17:30 by jiyokim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t			i;
+
+	i = 0;
+	if (dst == 0 && src == 0)
+		return (NULL);
+	if (n == 0)
+		return (dst);
+	while (i < n)
+	{
+		*((unsigned char *)dst + i) = *((unsigned char *)src + i);
+		i++;
+	}
+	return (dst);
+}
